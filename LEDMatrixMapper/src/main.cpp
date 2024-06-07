89be3bdb7c29355fecb47416dab8342d494b9822
JANC_PC\czech <czechumennakompie@gmail.com>
2024-06-08 00:00:43 +0200

    Change board, add main loop

--- LEDMatrixMapper/src/main.cpp
+++ LEDMatrixMapper/src/main.cpp
@@ -1,26 +1,50 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 9
+#define LED_COUNT 64
+
+struct Pattern {
+  int red[8][8];
+  int green[8][8];
+  int blue[8][8];
+};
 
 class LEDMatrixHandler{
 private:
 Pattern patterns[5]; // will change it so it is allocated dynamically on setup
 
-void mapPattern(Pattern p, int index);
+
+void displayPattern(int index);
 
 public:
 
+Adafruit_NeoPixel matrix;
+LEDMatrixHandler(Adafruit_NeoPixel m_matrix);
+LEDMatrixHandler();
 
 };
 
-struct Pattern {
-  int red[10][10];
-  int green[10][10];
-  int blue[10][10];
-};
+//LEDMatrixHandler _matrixHandler;
 
+Adafruit_NeoPixel* matrix;
 void setup() {
-  // put your setup code here, to run once:
+  Adafruit_NeoPixel m_matrix(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+  matrix = &m_matrix;
+  matrix->begin();
+  matrix->show();
+  matrix->setBrightness(64);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < 10; j++)
+    {
+      matrix->setPixelColor(i + j, (i * 2 + j), i, j);
+      matrix->show();
+    }
+    
+  }
 }
